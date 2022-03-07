31c84cc848341b5f9588a5dff4232b4d7fa8c4d6
enizeza <enizeza@live.it>
2022-03-07 11:30:39 +0100

    StaffTest fatto e librerie junit import sitemata

--- src/it/unipr/zezacracoliciTest/StaffTest.java
+++ src/it/unipr/zezacracoliciTest/StaffTest.java
@@ -3,23 +3,20 @@
  */
 package it.unipr.zezacracoliciTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Date;
 import java.sql.SQLException;
 
-import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.Test;
 
-import it.unipr.zezacracolici.Boat;
 import it.unipr.zezacracolici.Member;
 import it.unipr.zezacracolici.Race;
 import it.unipr.zezacracolici.Staff;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * StaffTest is in charge of testing the methods of the class Staff.
@@ -42,7 +39,7 @@ public class StaffTest {
     private Staff s;
 	
 	@Before
-	void setUp() throws Exception {
+	public void setUp() throws Exception {
 	}
 	
 	/**
@@ -57,47 +54,27 @@ public class StaffTest {
 	}
 	
 	
-	/*
-	@Test
-	public final void testPayOrganizationSum() throws SQLException {
-		m.payOrganizationSum(1, 1, 234);
-	}
-
-	@Test
-	public final void testPayBoatStorageSum() throws SQLException {
-		Boat b = new Boat(1,"Barbara",234);
-		m.payBoatStorageSum(b.getId(), 2, 256);
-	}
-
-	@SuppressWarnings("deprecation")
-	@Test
-	public final void testEnrollBoatRace() throws SQLException {
-		Date data = new Date(2022, 03, 23);
-		Race race = new Race(23,"La Ruleta","Monaco",data);
-		Boat b = new Boat(1,"Barbara",234);
-		m.enrollBoatRace(b.getId(), race.getIdrace(), 1, 234);
-	}
-
 	
 	@Test
-	final void testSendNotificationOrganization() {
-		fail("Not yet implemented"); // TODO
+	public void testSendNotificationStorage() throws SQLException {
+		Member m = new Member(ID, USERNAME, PASSWORD);
+		s.sendNotificationStorage(m.getId());
 	}
-
+	
+	@SuppressWarnings("deprecation")
 	@Test
-	final void testSendNotificationStorage() {
-		fail("Not yet implemented"); // TODO
+	public void testAddRace() throws SQLException {
+		Date data = new Date(2022, 03, 23);
+		Race race = new Race(23,"La Ruleta","Monaco",data);
+		s.addRace(race);
 	}
-
+	
+	@SuppressWarnings("deprecation")
 	@Test
-	final void testAddRace() {
-		fail("Not yet implemented"); // TODO
+	public void testRemoveRace() throws SQLException {
+		Date data = new Date(2022, 03, 23);
+		Race race = new Race(23,"La Ruleta","Monaco",data);
+		s.addRace(race);
+		s.removeRace(race.getIdrace());
 	}
-
-
-	@Test
-	final void testRemoveRace() {
-		fail("Not yet implemented"); // TODO
-	}*/
-
 }
